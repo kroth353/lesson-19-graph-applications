823dcdf772f9a515308c3e2f1e5ddc8e05e8b6e6
sam <sjglover@udel.edu>
2023-04-18 20:31:12 -0400

    Changed the names of the nodes

--- prims.py
+++ prims.py
@@ -3,28 +3,49 @@ import matplotlib.pyplot as plt
 
 G = nx.Graph() # create a new Graph
 
-# direct requirements get a single directed edge
-G.add_edge("Romance", "Comedy", weight=2)
-G.add_edge("Romance", "Zombie", weight=50)
-G.add_edge("Comedy", "Musical", weight=1)
-G.add_edge("Zombie", "Horror", weight=2)
-G.add_edge("Thriller", "Horror", weight=5)
-G.add_edge("Drama", "Romance", weight=7)
-G.add_edge("Drama", "Horror", weight=11)
-G.add_edge("Thriller", "Action", weight=9)
-G.add_edge("Zombie", "Fantasy", weight=1)
-G.add_edge("Crime", "Comedy", weight=8)
-G.add_edge("Romance", "Sports", weight=12)
-G.add_edge("Animation", "Drama", weight=5)
-G.add_edge("Sci Fi", "Thriller", weight=4)
-G.add_edge("Western", "Action", weight=2)
-G.add_edge("Historical", "Documentary", weight=5)
-G.add_edge("Anime", "Animation", weight=1)
-G.add_edge("Satire", "Drama", weight=3)
-G.add_edge("Documentary", "Western", weight=4)
-G.add_edge("Mystery", "Crime", weight=6)
-G.add_edge("Biographical", "Documentary", weight=3)
-G.add_edge("Thriller", "Fantasy", weight=3)
+"""
+A = Animation
+Ac = Action 
+An = Anime 
+B = Biographical 
+C = Comedy 
+Cr = Crime 
+D = Drama 
+Do = Documentary 
+F = Fantasy 
+H = Historical 
+Ho - Horror 
+M = Musical 
+My = Mystery 
+R = Romance 
+S = Satire
+Sc = Sci Fi  
+Sp = Sports
+T = Thriller 
+W = Western 
+Z = Zombie 
+"""
+
+G.add_edge("R", "C", weight=2)
+G.add_edge("C", "M", weight=1)
+G.add_edge("Z", "Ho", weight=2)
+G.add_edge("T", "Ho", weight=5)
+G.add_edge("D", "R", weight=7)
+G.add_edge("D", "Ho", weight=11)
+G.add_edge("T", "Ac", weight=9)
+G.add_edge("Z", "F", weight=1)
+G.add_edge("Cr", "C", weight=8)
+G.add_edge("R", "Sp", weight=12)
+G.add_edge("A", "D", weight=5)
+G.add_edge("Sc", "T", weight=4)
+G.add_edge("W", "Ac", weight=2)
+G.add_edge("H", "Do", weight=5)
+G.add_edge("An", "A", weight=1)
+G.add_edge("S", "D", weight=3)
+G.add_edge("Do", "W", weight=4)
+G.add_edge("My", "Cr", weight=6)
+G.add_edge("B", "Do", weight=3)
+G.add_edge("T", "F", weight=3)
 
 # draw the graph and show it MUST BE THE LAST TWO LINES
 pos = nx.spring_layout(G)  # positions for all nodes - seed for reproducibility
