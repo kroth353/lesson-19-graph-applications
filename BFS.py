4e2ba0e564d60f40fa63ca99d505cbf9bd608156
mmathews <mmathews@udel.edu>
2023-05-05 01:40:50 -0400

    got code to work correctly

--- BFS.py
+++ BFS.py
@@ -8,27 +8,6 @@ graph = nx.Graph()
 plt.figure(figsize=(9,9))
 
 # data for graph
-graph.add_edge('CP', 'JA')
-graph.add_edge('CP', 'JB')
-graph.add_edge('CP', 'RR')
-graph.add_edge('CP', 'MR')
-graph.add_edge('CP', 'OW')
-graph.add_edge('CP', 'VV')
-graph.add_edge('CP', 'IF')
-graph.add_edge('CP', 'JS')
-graph.add_edge('CP', 'ER')
-graph.add_edge('CP', 'MRK')
-graph.add_edge('CP', 'AH')
-graph.add_edge('CP', 'RW')
-graph.add_edge('JA', 'JB')
-graph.add_edge('JA', 'AS')
-graph.add_edge('JA', 'SJ')
-graph.add_edge('JA', 'OW')
-graph.add_edge('JA', 'VV')
-graph.add_edge('JA', 'IF')
-graph.add_edge('JA', 'JS')
-graph.add_edge('JA', 'ER')
-graph.add_edge('JA', 'MRK')
 graph.add_edge('JB', 'RR')
 graph.add_edge('JB', 'OW')
 graph.add_edge('JB', 'VV')
@@ -39,12 +18,11 @@ graph.add_edge('AS', 'RW')
 graph.add_edge('SJ', 'MR')
 graph.add_edge('SJ', 'JR')
 graph.add_edge('SJ', 'NP')
-graph.add_edge('SJ', 'MRK')
 graph.add_edge('SJ', 'AH')
 graph.add_edge('SJ', 'RW')
+graph.add_edge('SJ', 'Z')
 graph.add_edge('RR', 'OW')
 graph.add_edge('RR', 'IF')
-graph.add_edge('RR', 'MRK')
 graph.add_edge('RR', 'DF')
 graph.add_edge('MR', 'IF')
 graph.add_edge('MR', 'JL')
@@ -60,34 +38,60 @@ graph.add_edge('VV', 'NP')
 graph.add_edge('VV', 'DF')
 graph.add_edge('VV', 'RW')
 graph.add_edge('IF', 'JR')
-graph.add_edge('IF', 'MRK')
 graph.add_edge('IF', 'LD')
 graph.add_edge('IF', 'DF')
 graph.add_edge('JS', 'ER')
 graph.add_edge('JS', 'AH')
+graph.add_edge('JS', 'JH')
 graph.add_edge('ER', 'DF')
 graph.add_edge('ER', 'AH')
 graph.add_edge('JL', 'JR')
-graph.add_edge('JL', 'MRK')
 graph.add_edge('JL', 'LD')
+graph.add_edge('JL', 'JH')
+graph.add_edge('JL', 'SC')
 graph.add_edge('JR', 'NP')
-graph.add_edge('JR', 'MRK')
-graph.add_edge('NP', 'MRK')
-graph.add_edge('MRK', 'LD')
-graph.add_edge('MRK', 'DF')
-graph.add_edge('MRK', 'AH')
-graph.add_edge('MRK', 'RW')
+graph.add_edge('DF', 'JH')
+graph.add_edge('JH', 'SC')
+
+
 
 
 position = nx.spring_layout(graph)
 
 # nodes
-nx.drawNodes(graph, position, node_size=400)
+nx.draw_networkx_nodes(graph, position, node_size=400)
 
 # edges
-nx.drawEdges(graph, position, width=1)
+nx.draw_networkx_edges(graph, position, width=1)
 
 # node labels
-nx.drawNodeLabels(graph, position, font_size=10, font_family="sans-serif", font_weight="bold", font_color="white")
+nx.draw_networkx_labels(graph, position, font_size=10, font_family="sans-serif", font_weight="bold", font_color="white")
 
 plt.show()
+
+
+
+# Graph
+start_node = 'AS'
+end_node = 'JL'
+path = []
+
+bfs_tree = nx.bfs_tree(graph, start_node)
+path_nodes = nx.shortest_path(bfs_tree, source=start_node, target=end_node)
+
+path = []
+for i in range(0, len(path_nodes) - 1):
+    path.append(path_nodes[i])
+    path.append(path_nodes[i+1])
+
+# create a subgraph with shortest path from Adam Sandler and JH
+sub = graph.subgraph(path)
+
+# Draw the subgraph using matplotlib
+position = nx.spring_layout(graph)
+nx.draw(sub, position, with_labels=True)
+plt.show()
+
+finalPath = sorted(set(path), key=path.index)
+print(finalPath)
+print("Smallest Degree: " + str((len(finalPath) - 1)))
